221270743ddfa4045d9089b32f3ec5d844197f82
Chiahao Lin <y80811@gmail.com>
2018-01-18 13:55:31 +0800

    feat: add some examples to demo page

--- packages/dialog/src/App.js
+++ packages/dialog/src/App.js
@@ -6,13 +6,36 @@ const Fragment = React.Fragment
 
 class App extends Component {
   render() {
+    const style = { margin: '10px' }
     return (
       <Fragment>
-        <TextDialog visible onClose={() => alert('close dialog')}>
-          Hi, some content!
+        <TextDialog
+          style={style}  
+          visible
+          confirmText="Yes"
+          cancelText="No"
+          onClose={() => alert('close')}
+          onConfirm={() => alert('confirm')}
+          onCancel={() => alert('cancel')}
+        >
+          <p>
+            VeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongTextVeryLongText
+          </p>
         </TextDialog>
-        <PopupDialog style={{margin: '20px'}} visible level="info" confirmText="OK" />
-        <PopupDialog visible level="warning" confirmText="OK" />
+        <PopupDialog style={style} visible level="info" confirmText="OK" />
+        <PopupDialog style={style} visible level="warning" confirmText="OK" />
+        <PopupDialog style={style} visible level="error" confirmText="OK" />
+        <PopupDialog style={style} visible level="charging" confirmText="OK" />
+        <PopupDialog style={style} visible level="ok" confirmText="OK" />
+        <PopupDialog style={style} visible level="remind" confirmText="OK" />
+        <PopupDialog style={style} visible level="delete" confirmText="OK" />
+        <PopupDialog style={style} visible level="help" confirmText="OK" />
+        <PopupDialog
+          style={style}
+          visible
+          level="Error_but_system_still_can_process"
+          confirmText="OK"
+        />
       </Fragment>
     )
   }

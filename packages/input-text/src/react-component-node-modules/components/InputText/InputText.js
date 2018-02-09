2647ce97300a23f4052fe53f6b564a84a1b34bfc
Chun-Lin <wulin40063@gmail.com>
2018-02-09 14:56:46 +0800

    style: add margin width and max-width for the label text
    now users can assign titleMaxWidth for max-width of label text, also both side length of label text
    can be assigned by titleSpanR and titleSpanL
    
    ]

--- packages/input-text/src/react-component-node-modules/components/InputText/InputText.js
+++ packages/input-text/src/react-component-node-modules/components/InputText/InputText.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const Input = styled.input.attrs({
   type: 'text',
@@ -36,17 +37,44 @@ const Input = styled.input.attrs({
 `
 
 const Label = styled.label`
+  margin-left: ${props => props.titleSpanL}px;
+  margin-right: ${props => props.titleSpanR}px;
   &:after {
-    content: ' : ';
+    content: ' :';
   }
 `
-const InputText = ({ className, onChange, onClick, children}) => {
+
+const Title = styled.div`
+  display: inline-block;
+  max-width: ${props => props.titleMaxWidth}px;
+`
+
+const InputText = ({ className, onChange, onClick, children }) => {
   return (
     <div>
-      <Label htmlFor={children}>Test</Label>
-      <Input id={children} onChange={onChange} className={className}/>
+      <Title titleMaxWidth={200}>
+        <Label htmlFor={children} titleSpanL={0} titleSpanR={150}>
+          Test
+        </Label>
+      </Title>
+      <Input id={children} onChange={onChange} className={className} />
     </div>
   )
+}
+
+InputText.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  disabled: PropTypes.bool,
+  titleSpanR: PropTypes.number,
+  titleSpanL: PropTypes.number,
+  titleMaxWidth: PropTypes.number
+}
+
+InputText.defaultProps = {
+  value: '',
+  disabled: false,
+  titleSpanR: 10,
+  titleSpanL: 0,
 }
 
 export default InputText

5efb94343bda69d7b3650266647744ab99d5a21e
Chun-Lin <chun-lin@Chun-Lins-MacBook-Pro.local>
2018-03-23 18:17:12 +0800

    feat: initialization of Dropdown component

---
+++ packages/dropdown/src/react-component-node-modules/components/Dropdown/Dropdown.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Dropdown extends Component {
+  render() {
+    return (
+      <div>
+        <span>test</span>
+      </div>
+    )
+  }
+}
+
+export default Dropdown

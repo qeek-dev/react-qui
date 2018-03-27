29993ccab215a69c6e0bdc95a21aacce6182e35f
Chun-Lin <chun-lin@Chun-Lins-MacBook-Pro.local>
2018-03-27 15:01:25 +0800

    feat: add Label component to form directory
    now you can set className, htmlFor, form, children as props to <Label> component

---
+++ packages/form/label/src/react-component-node-modules/components/Label/Label.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Label = ({ className, htmlFor, children, form}) => {
+  return (
+    <label
+      className={className}
+      htmlFor={htmlFor}
+      form={form}
+    >
+      {children}
+    </label>
+  )
+}
+
+Label.propTypes = {
+  className: PropTypes.string,
+  htmlFor: PropTypes.string,
+  form: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.element,
+    PropTypes.array,
+  ]),
+}
+
+export default Label
---
+++ packages/form/label/src/react-component-node-modules/components/Label/Label.md
@@ -0,0 +1,13 @@
+## Label
+
+```js
+<Label>Test</Label>
+```
+
+```js
+<Label>Type Something <input type="text" id="User" name="Name" /></Label>
+```
+
+```js
+<Label>Click me <button>submit</button></Label>
+```

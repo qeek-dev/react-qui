21f8cff037df04995ffc7c993b1483efd572b435
Chun-Lin <wulin40063@gmail.com>
2018-03-28 18:33:22 +0800

    style: now the border of dropdown, arrow are fit in react-qui

--- packages/dropdown/src/react-component-node-modules/components/Dropdown/Dropdown.js
+++ packages/dropdown/src/react-component-node-modules/components/Dropdown/Dropdown.js
@@ -1,7 +1,50 @@
 import React, { Component } from 'react'
 import Select from 'react-select'
-import './styles/StyledDropdown.css'
+import './styles/OriginalDropdown.css'
 import styled from 'styled-components'
+import { rem } from 'polished'
+
+const StyledSelect = styled(Select)`
+  .Select-control {
+    border-radius: 0;
+    border: ${rem('1px')} solid #848484;
+    color: #2f2f2f;
+  }
+
+  .Select-control:hover {
+    box-shadow: none;
+  }
+
+  &&&.Select.is-open > .Select-control {
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+    background: transparent;
+    border-color: #000000;
+  }
+
+  .Select-arrow {
+    border-color: #2f2f2f transparent transparent;
+  }
+
+  &&&.Select .Select-arrow-zone:hover > .Select-arrow {
+    border-top-color: #2f2f2f;
+  }
+
+  &&&.Select.is-open > .Select-control .Select-arrow {
+    border-color: transparent transparent #2f2f2f;
+  }
+
+  .Select-menu-outer {
+    border-bottom-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+    border: ${rem('1px')} solid #000000;
+    box-shadow: none;
+  }
+
+  .Select-clear-zone {
+    display: none;
+  }
+`
 
 class Dropdown extends Component {
   state = {
@@ -15,7 +58,7 @@ class Dropdown extends Component {
     const value = selectedOption && selectedOption.value
 
     return (
-      <Select
+      <StyledSelect
         name="form-field-name"
         className={this.props.className}
         value={value}

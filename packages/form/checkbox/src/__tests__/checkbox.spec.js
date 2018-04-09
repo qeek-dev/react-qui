e69f22074bb0f9a45a0ee947c33b52c86236a69c
Chun-Lin <wulin40063@gmail.com>
2018-04-09 17:08:16 +0800

    test: add jest-styled-components to make snapshot clean

--- packages/form/checkbox/src/__tests__/checkbox.spec.js
+++ packages/form/checkbox/src/__tests__/checkbox.spec.js
@@ -4,6 +4,7 @@ import { mount,  } from 'enzyme'
 import { Checkbox } from '../react-component-node-modules/index'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import 'jest-styled-components'
 
 configure({ adapter: new Adapter() })
 

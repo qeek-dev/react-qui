374fdad19badae62fb50dab1e618b7973ad730bf
Chun-Lin <wulin40063@gmail.com>
2018-04-10 15:40:22 +0800

    fix: add missing component export module in index

--- packages/form/src/react-component-node-modules/index.js
+++ packages/form/src/react-component-node-modules/index.js
@@ -1,5 +1,7 @@
-export { default as Checkbox } from './components/Checkbox/Checkbox';
-export { default as CheckboxGroup } from './components/CheckboxGroup/CheckboxGroup';
-export { default as Label } from './components/Label/Label';
-export { default as InputText } from './components/InputText/InpuText';
-
+export { default as Checkbox } from './components/Checkbox/Checkbox'
+export {
+  default as CheckboxGroup,
+} from './components/CheckboxGroup/CheckboxGroup'
+export { default as Label } from './components/Label/Label'
+export { default as InputText } from './components/InputText/InputText'
+export { default as Qselect } from './components/Qselect/Qselect'

1f71a364e804e225da76593531378b8824a9d28f
Chun-Lin <wulin40063@gmail.com>
2018-04-10 11:34:15 +0800

    refactor: move InputText component into form project and delete original InputText compnonent

--- packages/form/src/react-component-node-modules/index.js
+++ packages/form/src/react-component-node-modules/index.js
@@ -1,2 +1,5 @@
 export { default as Checkbox } from './components/Checkbox/Checkbox';
 export { default as CheckboxGroup } from './components/CheckboxGroup/CheckboxGroup';
+export { default as Label } from './components/Label/Label';
+export { default as InputText } from './components/InputText/InpuText';
+
